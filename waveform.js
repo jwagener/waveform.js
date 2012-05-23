c681a4f26c392fc9eded6709349c7e55963d2003
Johannes Wagener <johannes@wagener.cc>
2012-05-22 23:03:41 -0700

    include compiled js

--- waveform.js
+++ waveform.js
@@ -1,6 +1,6 @@
 (function() {
   window.Waveform = function(options) {
-    var background, createCanvas, ctx, foreground, height, patchCanvasForIE, width;
+    var color, createCanvas, ctx, height, patchCanvasForIE, width;
     if (options == null) {
       options = {};
     }
@@ -28,8 +28,7 @@
     this.container = options.container;
     this.canvas = options.canvas;
     this.data = options.data || [];
-    background = options.background || "#ffff00";
-    foreground = options.foreground || "#0000ff";
+    color = options.color || "#0000ff";
     if (!(this.canvas != null)) {
       if (this.container) {
         if (this.canvas == null) {
@@ -48,8 +47,7 @@
       container: this.container,
       data: this.data,
       clear: function() {
-        ctx.fillStyle = background;
-        return ctx.fillRect(0, 0, width, height);
+        return ctx.clearRect(0, 0, width, height);
       },
       setDataWithLimit: function(data, limit, defaultValue) {
         var i, _ref, _results;
@@ -67,10 +65,44 @@
           return _results;
         }
       },
+      setDataInterpolated: function(data) {
+        var avg, c, i, interpolated, sauce, step, total;
+        total = data.length;
+        step = total / width;
+        interpolated = [];
+        avg = 0;
+        i = 0;
+        sauce = function() {
+          var diff, max, min;
+          min = 0.85;
+          max = 1.3;
+          diff = max - min;
+          return min + Math.random() * diff;
+        };
+        if (step < 1) {
+          c = 0;
+          while (i < width) {
+            interpolated.push(data[Math.round(c)] * sauce());
+            i += 1;
+            c += step;
+          }
+        } else {
+          step = Math.floor(step);
+          while (i < total) {
+            avg += data[i];
+            if (i % step === 0) {
+              interpolated.push(avg / step);
+              avg = 0;
+            }
+            i += 1;
+          }
+        }
+        return this.data = interpolated;
+      },
       redraw: function() {
         var d, i, middle, t, _i, _len, _ref, _results;
         this.clear();
-        ctx.fillStyle = foreground;
+        ctx.fillStyle = color;
         middle = height / 2;
         i = 0;
         _ref = this.data;
@@ -78,11 +110,7 @@
         for (_i = 0, _len = _ref.length; _i < _len; _i++) {
           d = _ref[_i];
           t = width / this.data.length;
-          if (foreground === "transparent") {
-            ctx.clearRect(t * i, middle - middle * d, t, middle * d * 2);
-          } else {
-            ctx.fillRect(t * i, middle - middle * d, t, middle * d * 2);
-          }
+          ctx.fillRect(t * i, middle - middle * d, t, middle * d * 2);
           _results.push(i++);
         }
         return _results;
